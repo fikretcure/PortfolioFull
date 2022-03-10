76d42291d15af1bdb1e1b4ad86aae5c8d1791e28
fikretcure <fikretcure@gmail.com>
2022-03-10 22:45:24 +0300

    title update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,29 +4,24 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
     <title>Fikret Cüre Resmi Web Sitesi</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-
     <!-- Favicons -->
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
         rel="stylesheet">
-
     <!-- Vendor CSS Files -->
     <link href="{{ asset('template') }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('template') }}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="{{ asset('template') }}/assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="{{ asset('template') }}/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="{{ asset('template') }}/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
     <!-- Variables CSS Files. Uncomment your preferred color scheme -->
     <link href="{{ asset('template') }}/assets/css/variables.css" rel="stylesheet">
     <!-- <link href="assets/css/variables-blue.css" rel="stylesheet"> -->
@@ -35,10 +30,8 @@
     <!-- <link href="assets/css/variables-purple.css" rel="stylesheet"> -->
     <!-- <link href="assets/css/variables-red.css" rel="stylesheet"> -->
     <!-- <link href="assets/css/variables-pink.css" rel="stylesheet"> -->
-
     <!-- Template Main CSS File -->
     <link href="{{ asset('template') }}/assets/css/main.css" rel="stylesheet">
-
     <!-- =======================================================
   * Template Name: HeroBiz - v2.1.0
   * Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
@@ -46,22 +39,14 @@
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 </head>
-
-
 <style type="text/css">
     .content {
-
-        background-color: rgb(248, 248, 248);
         width: 100%;
         height: 100vh;
         font-size: 25px;
-
-
         color: var(--color-secondary);
         font-family: var(--font-secondary);
     }
-
-
 
     .content .turkuaz {
         color: var(--color-primary);
@@ -73,38 +58,22 @@
         font-weight: 300;
         margin-left: 10px;
         margin-right: 10px;
-
-
     }
+
 </style>
 
 <body>
-
-
     <div class="content d-flex justify-content-center align-items-center">
-
         <span>
-           yakında
-
+            yakında
         </span>
         <span class="title">
             &#8750;i&#409;re&#2547;<span class="turkuaz">&#x212D;ure</span>
-
         </span>
         <span>
             sizlerle
-
-         </span>
-
-
-
-
+        </span>
     </div>
-
-
-
-
-
 </body>
 
 </html>
