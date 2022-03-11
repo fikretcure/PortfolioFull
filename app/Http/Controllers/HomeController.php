41eaa877cd678130b6023b94ab5bd506196dc2c4
fikretcure <fikretcure@gmail.com>
2022-03-11 20:27:27 +0300

    home update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view("frontend/home");
+
+        $repo = Http::withHeaders([
+            'Authorization' => env("GITHUB_TOKEN"),
+        ])->get('https://api.github.com/users/fikretcure/repos');
+
+        return view("frontend/home",["repo" => $repo->json()]);
     }
 }
