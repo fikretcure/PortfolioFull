4a14224e9b0006a02b130b59e3b1d219de4e8d98
fikretcure <fikretcure@gmail.com>
2022-03-12 12:05:37 +0300

    about image now

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -76,7 +76,7 @@
             <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
                 <div class="col-lg-5">
                     <div class="about-img">
-                        <img src="{{ asset('template/assets') }}/img/about.jpeg" class="img-fluid" alt="">
+                        <img src="{{ asset('template/assets') }}/img/about.jpeg?{{now()}}" class="img-fluid" alt="">
                     </div>
                 </div>
                 <div class="col-lg-7">
