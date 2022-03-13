452bac30b08c35a4786a082789dbb7f5208174a1
fikretcure <fikretcure@gmail.com>
2022-03-13 17:02:47 +0300

    message validate

--- app/Http/Controllers/sendMessageController.php
+++ app/Http/Controllers/sendMessageController.php
@@ -4,19 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class sendMessageController extends Controller
 {
     //
     public function send(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email:rfc,dns',
+            'name' => 'required',
+            'message' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors()->first();
+        }
+
         $data = [
             "name" => $request->name,
             "email" => $request->email,
             "mess" => $request->message,
         ];
-
-
 
         mail::send("sendmail", $data, function ($message) use ($request) {
             $message->subject($request->subject);
