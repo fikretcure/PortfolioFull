c571a78aec108ff9621a20f115b25af6c0c46ba5
fikretcure <fikretcure@gmail.com>
2022-03-20 21:41:40 +0300

    ip kısıtlama

--- app/Http/Middleware/GateWay.php
+++ app/Http/Middleware/GateWay.php
@@ -20,14 +20,28 @@ class GateWay
     {
         if (Location::get($request->ip()) &&  $request->ip() != "159.146.13.247" &&  $request->ip() != "77.245.159.2") {
             $client = Location::get($request->ip());
-            userRecords::create([
-                "country_name" => $client->countryName,
-                "city_name" => $client->cityName,
-                "server_addr" => $request->server("SERVER_ADDR"),
-                "remote_addr" => $request->server("REMOTE_ADDR"),
-                "route_name" => $request->route()->getName(),
-                "route_url" => $request->url(),
-            ]);
+
+            switch ($request->server("REMOTE_ADDR")) {
+                case '159.146.13.247':
+                case '77.245.159.2':
+                    break;
+
+                default:
+                    userRecords::create([
+                        "country_name" => $client->countryName,
+                        "city_name" => $client->cityName,
+                        "server_addr" => $request->server("SERVER_ADDR"),
+                        "remote_addr" => $request->server("REMOTE_ADDR"),
+                        "route_name" => $request->route()->getName(),
+                        "route_url" => $request->url(),
+                    ]);
+                    break;
+            }
+
+
+
+
+
             return $next($request);
         } else if (env("IP") == "dev") {
             return $next($request);
